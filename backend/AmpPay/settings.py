1d8166b92f50cb2ffa2124295f76a3e912c2ed09
aswinharikrishnan <aswinhkk@gmail.com>
2025-07-20 20:41:49 +0530

    updated

--- backend/AmpPay/settings.py
+++ backend/AmpPay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,8 +67,8 @@ ROOT_URLCONF = 'AmpPay.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'backend/AmpPay/templates')],  # Add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
--- backend/AmpPay/urls.py
+++ backend/AmpPay/urls.py
@@ -1,35 +1,96 @@
-"""
-URL configuration for AmpPay project.
+# """
+# URL configuration for AmpPay project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.0/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path
+# from .views import EnergyUsageList
+# from .views import receive_data
+# from .views import predictenergyconsumption
+# # from .views import send_data_to_arduino
+
+
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/energyusage/', EnergyUsageList.as_view(), name='energyusage-list-create'),
+#     path('api/receive-data/', receive_data, name='receive_data'),
+#     path('api/predictenergyconsumption',predictenergyconsumption, name='predictenergyconsumption'),
+#     # path('send_data_to_arduino/', send_data_to_arduino, name='send_data_to_arduino'),
+#     # path('', views.home_page),  # Add a root view
+# ]
+
+
+
+
+# from django.contrib import admin
+# from django.urls import path
+# from .views import (
+#     EnergyUsageList,
+#     receive_data,
+#     predictenergyconsumption,
+#     home_page  # New view for root URL
+# )
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/energyusage/', EnergyUsageList.as_view(), name='energyusage-list'),
+#     path('api/receive-data/', receive_data, name='receive-data'),
+#     path('api/predict-consumption/', predictenergyconsumption, name='predict-consumption'),
+#     path('', home_page, name='home'),  # New root view
+# ]
+
+
+
+
+# from django.contrib import admin
+# from django.urls import path
+# from .views import (
+#     EnergyUsageList,
+#     receive_data,
+#     predict_consumption,
+#     dashboard  # New frontend view
+# )
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+    
+#     # API Endpoints
+#     path('api/energyusage/', EnergyUsageList.as_view(), name='energy-usage'),
+#     path('api/receive-data/', receive_data, name='receive-data'),
+#     path('api/predict-consumption/', predict_consumption, name='predict-consumption'),
+    
+#     # Frontend Routes
+#     path('', dashboard, name='dashboard'),  # Main user dashboard
+# ]
+
+
 from django.contrib import admin
 from django.urls import path
-from .views import EnergyUsageList
-from .views import receive_data
-from .views import predictenergyconsumption
-# from .views import send_data_to_arduino
-
-
-
+from .views import (
+    EnergyUsageList,
+    receive_data,
+    predictenergyconsumption,
+    home_page  # New view we'll create
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/energyusage/', EnergyUsageList.as_view(), name='energyusage-list-create'),
-    path('api/receive-data/', receive_data, name='receive_data'),
-    path('api/predictenergyconsumption',predictenergyconsumption, name='predictenergyconsumption'),
-    # path('send_data_to_arduino/', send_data_to_arduino, name='send_data_to_arduino'),
-    # path('', views.home_page),  # Add a root view
+    path('api/energyusage/', EnergyUsageList.as_view(), name='energyusage-list'),
+    path('api/receive-data/', receive_data, name='receive-data'),
+    path('api/predict-consumption/', predictenergyconsumption, name='predict-consumption'),
+    path('', home_page, name='home'),  # New root route
 ]
-
